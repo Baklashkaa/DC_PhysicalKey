a8548598db52dac4c827124a592ff6e0f27dd451
Jodexx <0984134q@gmail.com>
2024-07-12 21:41:28 +0300

    fix args issue

--- src/main/java/net/baklashka/dc_physicalkey/commands/Commands.java
+++ src/main/java/net/baklashka/dc_physicalkey/commands/Commands.java
@@ -22,47 +22,49 @@ public class Commands implements SubCommand {
     }
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (args[0].equalsIgnoreCase("givekey") && sender.hasPermission("dc_physicalkey.give")) {
-            if (args.length <= 3) {
-                sender.sendMessage(ChatColor.GREEN + "=============HELP=============");
-                sender.sendMessage(ChatColor.BLUE + "/dc physicalkey givekey" + ChatColor.GREEN + " <player name> <case type> <amount> " + ChatColor.AQUA + "-" + ChatColor.DARK_GREEN + " give the player a physical key");
-                sender.sendMessage(ChatColor.BLUE + "/dc physicalkey reload " + ChatColor.AQUA + "-" + ChatColor.DARK_GREEN + " reload plugin config");
-            } else {
-                String playername = args[1];
-                String amountst = args[3];
-                String type = args[2];
-                int amount = Integer.parseInt(amountst);
-                Player p = Bukkit.getServer().getPlayer(playername);
-                ItemStack item = new ItemStack(Material.TRIPWIRE_HOOK, amount);
-                YamlConfiguration config = configManager.getFile();
-                if (config != null && config.contains("Key.physical-key-lore") && config.contains("Key.physical-key-name")) {
-                    String name = ChatColor.translateAlternateColorCodes('&', config.getString("Key.physical-key-name"));
-                    List<String> lore = config.getStringList("Key.physical-key-lore");
-                    lore.add(" ");
-                    lore.add(config.getString("Key.physical-key-lore-case-type") + ": " + type);
-                    ItemMeta meta = item.getItemMeta();
-                    if (meta != null) {
-                        meta.setDisplayName(name);
-                        meta.setLore(lore);
-                        item.setItemMeta(meta);
+        if(args.length >= 1) {
+            if (args[0].equalsIgnoreCase("givekey") && sender.hasPermission("dc_physicalkey.give")) {
+                if (args.length <= 3) {
+                    sender.sendMessage(ChatColor.GREEN + "=============HELP=============");
+                    sender.sendMessage(ChatColor.BLUE + "/dc physicalkey givekey" + ChatColor.GREEN + " <player name> <case type> <amount> " + ChatColor.AQUA + "-" + ChatColor.DARK_GREEN + " give the player a physical key");
+                    sender.sendMessage(ChatColor.BLUE + "/dc physicalkey reload " + ChatColor.AQUA + "-" + ChatColor.DARK_GREEN + " reload plugin config");
+                } else {
+                    String playername = args[1];
+                    String amountst = args[3];
+                    String type = args[2];
+                    int amount = Integer.parseInt(amountst);
+                    Player p = Bukkit.getServer().getPlayer(playername);
+                    ItemStack item = new ItemStack(Material.TRIPWIRE_HOOK, amount);
+                    YamlConfiguration config = configManager.getFile();
+                    if (config != null && config.contains("Key.physical-key-lore") && config.contains("Key.physical-key-name")) {
+                        String name = ChatColor.translateAlternateColorCodes('&', config.getString("Key.physical-key-name"));
+                        List<String> lore = config.getStringList("Key.physical-key-lore");
+                        lore.add(" ");
+                        lore.add(config.getString("Key.physical-key-lore-case-type") + ": " + type);
+                        ItemMeta meta = item.getItemMeta();
+                        if (meta != null) {
+                            meta.setDisplayName(name);
+                            meta.setLore(lore);
+                            item.setItemMeta(meta);
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "ERROR! (001)");
+                        }
                     } else {
-                        sender.sendMessage(ChatColor.RED + "ERROR! (001)");
+                        sender.sendMessage(ChatColor.RED + "ERROR! (002)");
                     }
-                } else {
-                    sender.sendMessage(ChatColor.RED + "ERROR! (002)");
-                }
-                if (p != null) { // Check if the player is not null
-                    p.getInventory().addItem(item);
-                    String amountstring = Integer.toString(amount);
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("give-key-message").replaceAll("%player%", playername).replaceAll("%amount%", amountstring)));
-                } else {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("player-not-found").replaceAll("%player%", playername)));
+                    if (p != null) { // Check if the player is not null
+                        p.getInventory().addItem(item);
+                        String amountstring = Integer.toString(amount);
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("give-key-message").replaceAll("%player%", playername).replaceAll("%amount%", amountstring)));
+                    } else {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("player-not-found").replaceAll("%player%", playername)));
+                    }
                 }
             }
-        }
-        if (args[0].equalsIgnoreCase("reload") && sender.hasPermission("dc_physicalkey.reload")) {
-            configManager.reloadConfig();
-            sender.sendMessage(ChatColor.GREEN + "The config has been successfully reloaded!");
+            if (args[0].equalsIgnoreCase("reload") && sender.hasPermission("dc_physicalkey.reload")) {
+                configManager.reloadConfig();
+                sender.sendMessage(ChatColor.GREEN + "The config has been successfully reloaded!");
+            }
         }
     }
 
